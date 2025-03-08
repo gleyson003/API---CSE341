4e6fecfe6ea4d53f734788853c3458e12515dde1
gleyson003 <lop23031@byui.edu>
2025-03-07 23:11:43 -0300

    created the controller and routes directory.

---
+++ controllers/lesson1.js
@@ -0,0 +1,7 @@
+const myRoute = (req, res) => {
+    res.send("Gleyson Lopes Developer");
+};
+
+module.exports = {
+    myRoute
+}
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-  res.send("Gleyson Lopes Developer");
-});
-
 const port = 3000
+
+app.use('/', require('./routes'))
  
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
