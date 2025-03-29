32838e34931de33dae4866af67244840531b9809
gleyson003 <lop23031@byui.edu>
2025-03-29 08:57:50 -0300

    fix import user file in auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GitHubStrategy = require("passport-github2").Strategy;
-const User = require("./models/User");
+const User = require("./models/user");
 require("dotenv").config();
 
 passport.use(new GitHubStrategy({
